ae20fb8e8614ff3c660bf8027c3262c7df4729c5
Kethan Vegunta <kethan@vegunta.com>
2024-04-19 20:11:11 -0700

    feat: remove soap library dependency

--- index.js
+++ index.js
@@ -1,122 +1,164 @@
-const soap = require('soap');
-const xml2json = require('xml2json');
+const xml2json = require("xml2json");
 
 class StudentVueClient {
-    constructor(username, password, client) {
-        this.username = username;
-        this.password = password;
+  constructor(username, password, endpoint) {
+    this.username = username;
+    this.password = password;
 
-        this.client = client;
+    this.endpoint = endpoint;
+  }
+
+  getMessages() {
+    return this._xmlJsonSerialize(this._makeServiceRequest("GetPXPMessages"));
+  }
+
+  getCalendar() {
+    return this._xmlJsonSerialize(this._makeServiceRequest("StudentCalendar"));
+  }
+
+  getAttendance() {
+    return this._xmlJsonSerialize(this._makeServiceRequest("Attendance"));
+  }
+
+  getGradebook(reportPeriod) {
+    let params = {};
+    if (typeof reportPeriod !== "undefined") {
+      params.ReportPeriod = reportPeriod;
     }
+    return this._xmlJsonSerialize(
+      this._makeServiceRequest("Gradebook", params)
+    );
+  }
 
-    getMessages() {
-        return this._xmlJsonSerialize(this._makeServiceRequest('GetPXPMessages'));
+  getClassNotes() {
+    return this._xmlJsonSerialize(this._makeServiceRequest("StudentHWNotes"));
+  }
+
+  getStudentInfo() {
+    return this._xmlJsonSerialize(this._makeServiceRequest("StudentInfo"));
+  }
+
+  getSchedule(termIndex) {
+    let params = {};
+    if (typeof termIndex !== "undefined") {
+      params.TermIndex = termIndex;
     }
+    return this._xmlJsonSerialize(
+      this._makeServiceRequest("StudentClassList", params)
+    );
+  }
 
-    getCalendar() {
-        return this._xmlJsonSerialize(this._makeServiceRequest('StudentCalendar'));
-    }
+  getSchoolInfo() {
+    return this._xmlJsonSerialize(
+      this._makeServiceRequest("StudentSchoolInfo")
+    );
+  }
 
-    getAttendance() {
-        return this._xmlJsonSerialize(this._makeServiceRequest('Attendance'));
-    }
+  listReportCards() {
+    return this._xmlJsonSerialize(
+      this._makeServiceRequest("GetReportCardInitialData")
+    );
+  }
 
-    getGradebook(reportPeriod) {
-        let params = {};
-        if (typeof reportPeriod !== 'undefined') {
-            params.ReportPeriod = reportPeriod;
-        }
-        return this._xmlJsonSerialize(this._makeServiceRequest('Gradebook', params));
-    }
+  getReportCard(documentGuid) {
+    return this._xmlJsonSerialize(
+      this._makeServiceRequest("GetReportCardDocumentData", {
+        DocumentGU: documentGuid,
+      })
+    );
+  }
 
-    getClassNotes() {
-        return this._xmlJsonSerialize(this._makeServiceRequest('StudentHWNotes'));
-    }
+  listDocuments() {
+    return this._xmlJsonSerialize(
+      this._makeServiceRequest("GetStudentDocumentInitialData")
+    );
+  }
 
-    getStudentInfo() {
-        return this._xmlJsonSerialize(this._makeServiceRequest('StudentInfo'));
-    }
+  getDocument(documentGuid) {
+    return this._xmlJsonSerialize(
+      this._makeServiceRequest("GetContentOfAttachedDoc", {
+        DocumentGU: documentGuid,
+      })
+    );
+  }
 
-    getSchedule(termIndex) {
-        let params = {};
-        if (typeof termIndex !== 'undefined') {
-            params.TermIndex = termIndex;
-        }
-        return this._xmlJsonSerialize(this._makeServiceRequest('StudentClassList', params));
-    }
+  _xmlJsonSerialize(servicePromise) {
+    return servicePromise.then((result) =>
+      xml2json.toJson(result[0].ProcessWebServiceRequestResult)
+    );
+  }
 
-    getSchoolInfo() {
-        return this._xmlJsonSerialize(this._makeServiceRequest('StudentSchoolInfo'));
-    }
+  async _makeServiceRequest(
+    methodName,
+    params = {},
+    serviceHandle = "PXPWebServices"
+  ) {
+    let paramStr = `<Parms>${Object.entries(params).reduce(
+      (acc, [key, value]) => `${acc}<${key}>${value}</${key}>`,
+      ""
+    )}</Parms>`;
+    paramStr = paramStr.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 
-    listReportCards() {
-        return this._xmlJsonSerialize(this._makeServiceRequest('GetReportCardInitialData'));
-    }
-
-    getReportCard(documentGuid) {
-        return this._xmlJsonSerialize(this._makeServiceRequest('GetReportCardDocumentData', { DocumentGU: documentGuid }));
-    }
-
-    listDocuments() {
-        return this._xmlJsonSerialize(this._makeServiceRequest('GetStudentDocumentInitialData'));
-    }
-
-    getDocument(documentGuid) {
-        return this._xmlJsonSerialize(this._makeServiceRequest('GetContentOfAttachedDoc', { DocumentGU: documentGuid }));
-    }
-
-    _xmlJsonSerialize(servicePromise) {
-        return servicePromise.then(result => xml2json.toJson(result[0].ProcessWebServiceRequestResult));
-    }
-
-    _makeServiceRequest(methodName, params = {}, serviceHandle = 'PXPWebServices') {
-        let paramStr = '&lt;Parms&gt;';
-        Object.entries(params).forEach(([key, value]) => {
-            paramStr += '&lt;' + key + '&gt;';
-            paramStr += value;
-            paramStr += '&lt;/' + key + '&gt;';
-        });
-        paramStr += '&lt;/Parms&gt;';
-
-        return this.client.ProcessWebServiceRequestAsync({
-            userID: this.username,
-            password: this.password,
-            skipLoginLog: 1,
-            parent: 0,
-            webServiceHandleName: serviceHandle,
-            methodName,
-            paramStr
-        });
-    }
+    return await fetch(this.endpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/soap+xml; charset=utf-8" },
+      body: `<?xml version="1.0" encoding="utf-8"?>
+            <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                <soap12:Body>
+                    <ProcessWebServiceRequest xmlns="http://edupoint.com/webservices/">
+                        <userID>${this.userID}</userID>
+                        <password>${this.password}</password>
+                        <skipLoginLog>true</skipLoginLog>
+                        <parent>false</parent>
+                        <webServiceHandleName>${serviceHandle}</webServiceHandleName>
+                        <methodName>${methodName}</methodName>
+                        <paramStr>${paramStr}</paramStr>
+                    </ProcessWebServiceRequest>
+                </soap12:Body>
+            </soap12:Envelope>`
+        .split("\n")
+        .map((s) => s.trim())
+        .join("\n"),
+    });
+  }
 }
 
-function login(url, username, password, soapOptions = {}) {
-    const host = new URL(url).host;
-    const endpoint = `https://${ host }/Service/PXPCommunication.asmx`;
+function login(url, username, password) {
+  const host = new URL(url).host;
+  const endpoint = `https://${host}/Service/PXPCommunication.asmx`;
 
-    const resolvedOptions = Object.assign({
-        endpoint: endpoint, // enforces https
-        escapeXML: false
-    }, soapOptions);
+  const wsdlURL = endpoint + "?WSDL";
 
-    const wsdlURL = endpoint + '?WSDL';
-
-    return soap.createClientAsync(wsdlURL, resolvedOptions)
-        .then(client => new StudentVueClient(username, password, client));
+  return new StudentVueClient(username, password, wsdlURL);
 }
 
 function getDistrictUrls(zipCode) {
-    return soap.createClientAsync('https://support.edupoint.com/Service/HDInfoCommunication.asmx?WSDL', {
-        endpoint: 'https://support.edupoint.com/Service/HDInfoCommunication.asmx',
-        escapeXML: false
-    })
-        .then(client => {
-            const supportClient = new StudentVueClient('EdupointDistrictInfo', 'Edup01nt', client);
-            return supportClient._xmlJsonSerialize(supportClient._makeServiceRequest('GetMatchingDistrictList', {
-                MatchToDistrictZipCode: zipCode,
-                Key: '5E4B7859-B805-474B-A833-FDB15D205D40' // idk how safe this is
-            }, 'HDInfoServices'));
-        });
+  return soap
+    .createClientAsync(
+      "https://support.edupoint.com/Service/HDInfoCommunication.asmx?WSDL",
+      {
+        endpoint:
+          "https://support.edupoint.com/Service/HDInfoCommunication.asmx",
+        escapeXML: false,
+      }
+    )
+    .then((client) => {
+      const supportClient = new StudentVueClient(
+        "EdupointDistrictInfo",
+        "Edup01nt",
+        client
+      );
+      return supportClient._xmlJsonSerialize(
+        supportClient._makeServiceRequest(
+          "GetMatchingDistrictList",
+          {
+            MatchToDistrictZipCode: zipCode,
+            Key: "5E4B7859-B805-474B-A833-FDB15D205D40", // idk how safe this is
+          },
+          "HDInfoServices"
+        )
+      );
+    });
 }
 
 module.exports = { login, getDistrictUrls };
